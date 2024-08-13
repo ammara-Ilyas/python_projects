2cceae55497af8e5968e19fea36935eec0998ba8
ammara-Ilyas <ismarailyas545@gmail.com>
2024-08-13 11:39:57 +0500

    Alhumdulilah Python first project is completed

--- hotel_menu.py
+++ hotel_menu.py
@@ -11,7 +11,6 @@ print("Welcome to our Resturant")
 for item,price in menu.items():
     print(f"{item} : {price}")
 
-
 total_price=0
 def ordered_list(item,price):
     item=item.capitalize()
@@ -21,24 +20,26 @@ def ordered_list(item,price):
        print(f"{item} has been added in order list and total price is {price}")
     else:
        print(f"Ordered {item} is not available")   
+    return price
 
-item_1=input("Enter your first order : ")
-
-ordered_list(item_1,total_price)
-
-print("total",total_price)
-more_order=input("Do you want to add another items Y for yes /N for No")
-more_order=more_order.lower()
-if(more_order == "y" or more_order == "yes"):
-    item=input("Enter the second item")
-    ordered_list(item,total_price)
-
-# item_1=item_1.capitalize()
-# print(item_1) 
-# if item_1 in menu:
-#     total_price=total_price+menu[item_1]
-#     print(f"{item_1} has been added in order list and total price is {total_price}")
-# else:
-#     print(f"Ordered {item_1} is not available")   
+def more_ordered(current_price):
+   while True:
+     more_order=input("Do you want to add another items Y for yes or N for No ")
+     more_order=more_order.lower()
+     if more_order in ["yes","y"]:
+      item=input("Enter the next item ")
     
+      current_price= ordered_list(item,current_price)
+      print("current",current_price)
+     elif more_order in ["no","n"]:
+    #   print("current",current_price)
+      return current_price
+     else:
+       print("Enter yes or not ")
+       
     
+item_1=input("Enter your first order ")
+total_price=ordered_list(item_1,total_price)
+total_price = more_ordered(total_price)
+print("Thank you")
+print(f"The total price for all your orders is: {total_price}")
